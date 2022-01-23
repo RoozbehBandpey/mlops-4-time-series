3fce71b54ae8c5aac46cac994b099e79e6dc2d20
Roozbeh Bandpey <roozbeh.bandpey@accenture.com>
2022-01-23 13:33:48 +0000

    create dsetup script

--- azureml_utils/workspace_helper.py
+++ azureml_utils/workspace_helper.py
@@ -3,6 +3,10 @@ import logging
 from azureml.core import Workspace
 from azureml.core import Keyvault
 from azureml.core.authentication import AzureCliAuthentication, MsiAuthentication, InteractiveLoginAuthentication, ServicePrincipalAuthentication
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+
 
 _logger = logging.getLogger(__name__)
 
@@ -16,34 +20,30 @@ class WorkspaceHelper():
 		- ...
 	"""
 
-	def __init__(self, ws_config_file='config.json'):
+	def __init__(self, ws_config_file='config.json', auth_type='sp'):
 		"""Initializes an AML workspace object"""
+		self.ws_config_file = ws_config_file
+		
+		if auth_type='sp':
+			auth = ServicePrincipalAuthentication(tenant_id=os.environ.get("TENANT_ID"), service_principal_id=os.environ.get("SP_ID"), service_principal_password=os.environ.get("SP_PASSWORD"))
+		else if auth_type='int':
+			auth=InteractiveLoginAuthentication(tenant_id=os.environ.get("TENANT_ID"))
+		else if auth_type='cli':
+			auth=AzureCliAuthentication()
+		else if auth_type='msi':
+			auth=MsiAuthentication()
+		else:
+			raise(Exception)
+		
 		self.ws = Workspace.from_config(
-			# auth=InteractiveLoginAuthentication(tenant_id='ffbaf41d-7dbb-476e-bd38-2c12e2c319f7'), # TODO: Replace with MsiAuthentication for production workload
-			auth=AzureCliAuthentication(), # TODO: Replace with MsiAuthentication for production workload
-			# auth=MsiAuthentication(), # TODO: Replace with MsiAuthentication for production workload
+			auth=auth,
 			path=os.path.join(
 				os.path.dirname(os.path.realpath(__file__)),
-				ws_config_file
+				self.ws_config_file
 			)
 		)
 		print(f"Found workspace {self.ws.name} \n\tat location {self.ws.location}\n\t with the id:{self.ws._workspace_id}")
 
-	def authenticate_with_sp(self, ws_config_file='config.json'):
-		keyvault = self.ws.get_default_keyvault()
-		sp_tenant_id = keyvault.get_secret(name='sp-tenant-id')
-		sp_client_id = keyvault.get_secret(name='sp-client-id')
-		sp_password = keyvault.get_secret(name='sp-password')
-
-		sp_auth = ServicePrincipalAuthentication(tenant_id=sp_tenant_id, service_principal_id=sp_client_id, service_principal_password=sp_password)
-		print(f"Authenticated with Service Principal with session: \n\t{sp_auth.signed_session()}")
-		self.ws = Workspace.from_config(
-			auth=sp_auth,
-			path=os.path.join(
-				os.path.dirname(os.path.realpath(__file__)),
-				ws_config_file
-			)
-		)
 
 	def get_workspace(self):
 		return self.ws
